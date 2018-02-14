274c010c03eef6a5c981c883c798efa4690db97f
Admin <Admin@MARIO-PC>
2018-02-14 23:45:29 +0100

    Initial commit

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		
+	}
+	
+	public static void main(String[] args) {
+		new HelloWorld();
+	}
+
+}
