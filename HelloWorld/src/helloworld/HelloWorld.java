3e87b430bb35995f46063ce805c6cba3db9eacef
Admin <Admin@MARIO-PC>
2018-02-15 00:03:32 +0100

    Commit de la V2

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		System.out.println("= V1 =");
-		
+		System.out.println("= V2 =");
 	}
 	
 	public static void main(String[] args) {
