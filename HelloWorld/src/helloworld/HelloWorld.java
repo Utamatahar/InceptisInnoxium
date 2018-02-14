11881fd06f6db1e92317cf20c0c067c2be6b0995
Admin <Admin@MARIO-PC>
2018-02-15 00:10:25 +0100

    Commit de la V4

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 		System.out.println("= V1 =");
 		System.out.println("= V2 =");
 		System.out.println("= V3 =");
+		System.out.println("= V4 =");
 	}
 	
 	public static void main(String[] args) {
